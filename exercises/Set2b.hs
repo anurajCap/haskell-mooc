d82c97db1fff2ec544eee7f6b4e833b2c43465f9
Anuraj Tapidas <anuraj.tapidas@capgemini.com>
2024-12-20 14:34:52 +0000

    set 2b

--- exercises/Set2b.hs
+++ exercises/Set2b.hs
@@ -1,9 +1,8 @@
 module Set2b where
 
-import Mooc.Todo
-
 -- Some imports you'll need. Don't add other imports :)
 import Data.List
+import Mooc.Todo
 
 ------------------------------------------------------------------------------
 -- Ex 1: compute binomial coefficients using recursion. Binomial
@@ -16,7 +15,9 @@ import Data.List
 -- Hint! pattern matching is your friend.
 
 binomial :: Integer -> Integer -> Integer
-binomial = todo
+binomial n 0 = 1
+binomial 0 k = if k > 0 then 0 else binomial (-1) k + binomial (-1) (k - 1)
+binomial n k = binomial (n - 1) k + binomial (n - 1) (k - 1)
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the odd factorial function. Odd factorial is like
@@ -27,17 +28,25 @@ binomial = todo
 --   oddFactorial 6 ==> 5*3*1 ==> 15
 
 oddFactorial :: Integer -> Integer
-oddFactorial = todo
+oddFactorial (-1) = 1
+oddFactorial x
+  | even x = oddFactorial (x - 1)
+  | otherwise = x * oddFactorial (x - 2)
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the Euclidean Algorithm for finding the greatest
 -- common divisor:
 --
 -- Given two numbers, a and b,
+
 -- * if one is zero, return the other number
+
 -- * if not, subtract the smaller number from the larger one
+
 -- * replace the larger number with this new number
+
 -- * repeat
+
 --
 -- For example,
 --   myGcd 9 12 ==> 3
@@ -56,10 +65,15 @@ oddFactorial = todo
 --   0      3
 --
 -- Background reading:
+
 -- * https://en.wikipedia.org/wiki/Euclidean_algorithm
 
 myGcd :: Integer -> Integer -> Integer
-myGcd = todo
+myGcd a 0 = a
+myGcd 0 a = a
+myGcd a b
+  | a >= b = myGcd (a - b) b
+  | a < b = myGcd (b - a) a
 
 ------------------------------------------------------------------------------
 -- Ex 4: Implement the function leftpad which adds space characters
@@ -71,11 +85,15 @@ myGcd = todo
 --   leftpad "xxxxx" 3 ==> "xxxxx"
 --
 -- Tips:
+
 -- * you can combine strings with the ++ operator.
+
 -- * you can compute the length of a string with the length function
 
 leftpad :: String -> Int -> String
-leftpad = todo
+leftpad s x
+  | length s < x = leftpad (" " ++ s) x
+  | otherwise = s
 
 ------------------------------------------------------------------------------
 -- Ex 5: let's make a countdown for a rocket! Given a number, you
@@ -86,12 +104,19 @@ leftpad = todo
 --   countdown 4 ==> "Ready! 4... 3... 2... 1... Liftoff!"
 --
 -- Hints:
+
 -- * you can combine strings with the ++ operator
+
 -- * you can use the show function to convert a number into a string
+
 -- * you'll probably need a recursive helper function
 
 countdown :: Integer -> String
-countdown = todo
+countdown x = "Ready! " ++ countdown' x
+
+countdown' :: Integer -> String
+countdown' 0 = "Liftoff!"
+countdown' x = show x ++ "... " ++ countdown' (x - 1)
 
 ------------------------------------------------------------------------------
 -- Ex 6: implement the function smallestDivisor that returns the
@@ -109,7 +134,9 @@ countdown = todo
 -- Hint: remember the mod function!
 
 smallestDivisor :: Integer -> Integer
-smallestDivisor = todo
+smallestDivisor x = smallestDivisor' x 2
+
+smallestDivisor' x k = if x `mod` k == 0 then k else smallestDivisor' x (k + 1)
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement a function isPrime that checks if the given number
@@ -118,7 +145,9 @@ smallestDivisor = todo
 -- Ps. 0 and 1 are not prime numbers
 
 isPrime :: Integer -> Bool
-isPrime = todo
+isPrime 0 = False
+isPrime 1 = False
+isPrime x = smallestDivisor x == x
 
 ------------------------------------------------------------------------------
 -- Ex 8: implement a function biggestPrimeAtMost that returns the
@@ -133,4 +162,4 @@ isPrime = todo
 --   biggestPrimeAtMost 10 ==> 7
 
 biggestPrimeAtMost :: Integer -> Integer
-biggestPrimeAtMost = todo
+biggestPrimeAtMost x = if isPrime x then x else biggestPrimeAtMost (x - 1)
